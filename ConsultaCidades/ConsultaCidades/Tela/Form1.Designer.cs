5ed44f0925634e2b4391653cabfdbe39d8120bf7
Vanderson da Silva Quintino <46028991+vandersonsq@users.noreply.github.com>
2021-08-24 18:43:32 -0300

    interface final

--- ConsultaCidades/ConsultaCidades/Tela/Form1.Designer.cs
+++ ConsultaCidades/ConsultaCidades/Tela/Form1.Designer.cs
@@ -29,46 +29,170 @@
         private void InitializeComponent()
         {
             this.btCodigoIbge = new System.Windows.Forms.Button();
-            this.txtCodigoIbge = new System.Windows.Forms.TextBox();
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.lbUf = new System.Windows.Forms.Label();
             this.lbCidade = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbLogradouro = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbBairro = new System.Windows.Forms.Label();
+            this.lbend = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btCodigoIbge
             // 
-            this.btCodigoIbge.Location = new System.Drawing.Point(162, 96);
+            this.btCodigoIbge.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btCodigoIbge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.btCodigoIbge.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btCodigoIbge.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btCodigoIbge.FlatAppearance.BorderSize = 25;
+            this.btCodigoIbge.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btCodigoIbge.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btCodigoIbge.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCodigoIbge.ForeColor = System.Drawing.Color.White;
+            this.btCodigoIbge.Location = new System.Drawing.Point(284, 44);
             this.btCodigoIbge.Name = "btCodigoIbge";
-            this.btCodigoIbge.Size = new System.Drawing.Size(134, 33);
+            this.btCodigoIbge.Size = new System.Drawing.Size(92, 26);
             this.btCodigoIbge.TabIndex = 0;
             this.btCodigoIbge.Text = "buscar";
-            this.btCodigoIbge.UseVisualStyleBackColor = true;
+            this.btCodigoIbge.UseVisualStyleBackColor = false;
             this.btCodigoIbge.Click += new System.EventHandler(this.btCodigoIbge_Click);
+            this.btCodigoIbge.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btCodigoIbge_MouseClick);
             // 
-            // txtCodigoIbge
+            // txtCep
             // 
-            this.txtCodigoIbge.Location = new System.Drawing.Point(162, 70);
-            this.txtCodigoIbge.Name = "txtCodigoIbge";
-            this.txtCodigoIbge.Size = new System.Drawing.Size(134, 20);
-            this.txtCodigoIbge.TabIndex = 1;
+            this.txtCep.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCep.Location = new System.Drawing.Point(197, 44);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(81, 26);
+            this.txtCep.TabIndex = 1;
+            this.txtCep.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCep_KeyPress);
+            // 
+            // lbUf
+            // 
+            this.lbUf.AutoSize = true;
+            this.lbUf.BackColor = System.Drawing.Color.Transparent;
+            this.lbUf.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUf.ForeColor = System.Drawing.Color.Black;
+            this.lbUf.Location = new System.Drawing.Point(227, 158);
+            this.lbUf.Name = "lbUf";
+            this.lbUf.Size = new System.Drawing.Size(19, 20);
+            this.lbUf.TabIndex = 2;
+            this.lbUf.Text = "uf";
+            this.lbUf.Click += new System.EventHandler(this.lbCidade_Click);
             // 
             // lbCidade
             // 
             this.lbCidade.AutoSize = true;
-            this.lbCidade.Location = new System.Drawing.Point(189, 189);
+            this.lbCidade.BackColor = System.Drawing.Color.Transparent;
+            this.lbCidade.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCidade.ForeColor = System.Drawing.Color.Black;
+            this.lbCidade.Location = new System.Drawing.Point(226, 185);
             this.lbCidade.Name = "lbCidade";
-            this.lbCidade.Size = new System.Drawing.Size(21, 13);
-            this.lbCidade.TabIndex = 2;
-            this.lbCidade.Text = "UF";
-            this.lbCidade.Click += new System.EventHandler(this.lbCidade_Click);
+            this.lbCidade.Size = new System.Drawing.Size(50, 20);
+            this.lbCidade.TabIndex = 3;
+            this.lbCidade.Text = "cidade";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(216, 189);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(206, 158);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "CIDADE";
+            this.label1.Size = new System.Drawing.Size(25, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Uf:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(175, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cidade:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(86, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Digite o CEP:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(149, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Logradouro:";
+            // 
+            // lbLogradouro
+            // 
+            this.lbLogradouro.AutoSize = true;
+            this.lbLogradouro.BackColor = System.Drawing.Color.Transparent;
+            this.lbLogradouro.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogradouro.ForeColor = System.Drawing.Color.Black;
+            this.lbLogradouro.Location = new System.Drawing.Point(226, 217);
+            this.lbLogradouro.Name = "lbLogradouro";
+            this.lbLogradouro.Size = new System.Drawing.Size(74, 20);
+            this.lbLogradouro.TabIndex = 7;
+            this.lbLogradouro.Text = "logradouro";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(183, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Bairro:";
+            // 
+            // lbBairro
+            // 
+            this.lbBairro.AutoSize = true;
+            this.lbBairro.BackColor = System.Drawing.Color.Transparent;
+            this.lbBairro.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbBairro.ForeColor = System.Drawing.Color.Black;
+            this.lbBairro.Location = new System.Drawing.Point(226, 247);
+            this.lbBairro.Name = "lbBairro";
+            this.lbBairro.Size = new System.Drawing.Size(43, 20);
+            this.lbBairro.TabIndex = 9;
+            this.lbBairro.Text = "bairro";
+            // 
+            // lbend
+            // 
+            this.lbend.AutoSize = true;
+            this.lbend.BackColor = System.Drawing.Color.Transparent;
+            this.lbend.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbend.ForeColor = System.Drawing.Color.Black;
+            this.lbend.Location = new System.Drawing.Point(136, 111);
+            this.lbend.Name = "lbend";
+            this.lbend.Size = new System.Drawing.Size(189, 22);
+            this.lbend.TabIndex = 11;
+            this.lbend.Text = "Endereço Localizado";
             // 
             // telaMenu
             // 
@@ -76,12 +200,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(477, 292);
+            this.Controls.Add(this.lbend);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbBairro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbLogradouro);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbCidade);
-            this.Controls.Add(this.txtCodigoIbge);
+            this.Controls.Add(this.lbUf);
+            this.Controls.Add(this.txtCep);
             this.Controls.Add(this.btCodigoIbge);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "telaMenu";
-            this.Text = "Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buscar endereço";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -91,9 +226,17 @@
         #endregion
 
         private System.Windows.Forms.Button btCodigoIbge;
-        private System.Windows.Forms.TextBox txtCodigoIbge;
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label lbUf;
         private System.Windows.Forms.Label lbCidade;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbLogradouro;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbBairro;
+        private System.Windows.Forms.Label lbend;
     }
 }
 
--- ConsultaCidades/ConsultaCidades/Tela/Form1.cs
+++ ConsultaCidades/ConsultaCidades/Tela/Form1.cs
@@ -11,73 +11,136 @@ namespace ConsultaCidades
         public telaMenu()
         {
             InitializeComponent();
+            desativarCampos();
+        }
+        public void desativarCampos()
+        {
+            lbend.Visible = false;
+            lbUf.Visible = false;
+            lbLogradouro.Visible = false;
+            lbBairro.Visible = false;
+            lbCidade.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label4.Visible = false;
+            label6.Visible = false;
+        }
+        public void ativarCampos()
+        {
+            lbend.Visible = true;
+            lbUf.Visible = true;
+            lbLogradouro.Visible = true;
+            lbBairro.Visible = true;
+            lbCidade.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label4.Visible = true;
+            label6.Visible = true;
         }
         private void btCodigoIbge_Click(object sender, EventArgs e)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + txtCodigoIbge.Text + "/json/");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + txtCep.Text + "/json/");
             request.AllowAutoRedirect = false;
-            HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
 
-            if (ChecaServidor.StatusCode != HttpStatusCode.OK)
+            try
             {
-                //MessageBox.Show("Servidor indisponível");
-                return; // Sai da rotina
-            }
+                HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
 
-            using (Stream webStream = ChecaServidor.GetResponseStream())
-            {
-                if (webStream != null)
+                if (ChecaServidor.StatusCode != HttpStatusCode.OK)
                 {
-                    using (StreamReader responseReader = new StreamReader(webStream))
+                    MessageBox.Show("Servidor indisponível, tente novamente!");
+                    return; // Sai da rotina
+                }
+                using (Stream webStream = ChecaServidor.GetResponseStream())
+                {
+                    if (webStream != null)
                     {
-                        string response = responseReader.ReadToEnd();
-                        response = Regex.Replace(response, "[{},]", string.Empty);
-                        response = response.Replace("\"", "");
-
-                        String[] substrings = response.Split('\n');
-
-                        int cont = 0;
-                        foreach (var substring in substrings)
+                        using (StreamReader responseReader = new StreamReader(webStream))
                         {
-                            if (cont == 1)
+                            string response = responseReader.ReadToEnd();
+                            response = Regex.Replace(response, "[{},]", string.Empty);
+                            response = response.Replace("\"", "");
+
+                            String[] substrings = response.Split('\n');
+
+                            int cont = 0;
+
+                            txtCep.Enabled = false;
+                            btCodigoIbge.Enabled = false;
+
+                            foreach (var substring in substrings)
                             {
-                                string[] valor = substring.Split(":".ToCharArray());
-                                if (valor[0] == "  erro")
+                                if (cont == 1)
                                 {
-                                    //MessageBox.Show("CEP não encontrado");
-                                    txtCep.Focus();
-                                    return;
+                                    string[] valor = substring.Split(":".ToCharArray());
+                                    if (valor[0] == "  erro")
+                                    {
+                                        MessageBox.Show("CEP não encontrado");
+                                        txtCep.Focus();
+                                        return;
+                                    }
                                 }
-                            }
+                                //Logradouro
+                                if (cont == 2)
+                                {
+                                    string[] valor = substring.Split(":".ToCharArray());
+                                    lbLogradouro.Text = valor[1];
+                                }
 
-                            //Localidade (Cidade)
-                            if (cont == 5)
-                            {
-                                string[] valor = substring.Split(":".ToCharArray());
-                                txtLocalidade.Text = valor[1];
-                            }
+                                //Bairro
+                                if (cont == 4)
+                                {
+                                    string[] valor = substring.Split(":".ToCharArray());
+                                    lbBairro.Text = valor[1];
+                                }
 
-                            //Estado (UF)
-                            if (cont == 6)
-                            {
-                                string[] valor = substring.Split(":".ToCharArray());
-                                txtUF.Text = valor[1];
+                                //Localidade (Cidade)
+                                if (cont == 5)
+                                {
+                                    string[] valor = substring.Split(":".ToCharArray());
+                                    lbCidade.Text = valor[1];
+                                }
+                                //Estado (UF)
+                                if (cont == 6)
+                                {
+                                    string[] valor = substring.Split(":".ToCharArray());
+                                    lbUf.Text = valor[1];
+                                }
+                                cont++;
+                                ativarCampos();
                             }
-
-                            cont++;
+                            txtCep.Enabled = true;
+                            btCodigoIbge.Enabled = true;
                         }
-
                     }
                 }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("O CEP digitado está no formato inválido!");
+            }        
+        }
+        private void txtCep_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)8)
+            {
+                e.Handled = true;
             }
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
         private void lbCidade_Click(object sender, EventArgs e)
         {
+        }
 
+        private void btCodigoIbge_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (lbUf.Visible == false)
+            {
+                txtCep.Enabled = true;
+                btCodigoIbge.Enabled = true;
+            }    
         }
     }  
 }
